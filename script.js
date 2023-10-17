0ffe08787489f491907683f8a94656c31d61b4f8
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-17 02:34:42 -0600

    move projectlist to index.html

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const projectList = document.createElement("p");
-document.querySelector("body").appendChild(projectList)
-projectList.id = "projectList"
-
-projectList.textContent = "LIST ALL PROJECTS HERE"
-
-
 const bio = document.createElement("p");
 document.querySelector("body").appendChild(bio)
 bio.id = "bio"
 
-bio.textContent = "Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to work for a non-profit and solve real world problems with my new skills."
+bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to work for a non-profit and solve real world problems with my new skills. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
+
 
