0231d49cb07bdbc76dde12a2290c189991bac2dc
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-17 02:20:49 -0600

    got bio and project list in script.js, added ids to projectlist and bio, and floated bio to the right

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const p = document.createElement("p");
-p.textContent = "LIST ALL PROJECTS HERE"
+const projectList = document.createElement("p");
+document.querySelector("body").appendChild(projectList)
+projectList.id = "projectList"
 
-document.querySelector("body").appendChild(p)
+projectList.textContent = "LIST ALL PROJECTS HERE"
+
+
+const bio = document.createElement("p");
+document.querySelector("body").appendChild(bio)
+bio.id = "bio"
+
+bio.textContent = "Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to work for a non-profit and solve real world problems with my new skills."
+
