b968b54475b3be8fc117ae8a3efce6240886c94e
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-19 20:53:10 -0600

    lots of text position changes, added images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const bio = document.createElement("p");
-document.querySelector("body").appendChild(bio)
+const bio = document.createElement("div");
+document.querySelector("div").appendChild(bio)
 bio.id = "bio"
 
-bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to work for a non-profit and solve real world problems with my new skills. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
+bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to find a position using my new skills to solve real world problems. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
 
 
