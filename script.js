be174bfd77fa4eb199b54b801cfdb76f497ab080
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-20 09:33:07 -0600

    eventlistener opacity changes and progress on event listener text

--- script.js
+++ script.js
@@ -4,4 +4,37 @@ bio.id = "bio"
 
 bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to find a position using my new skills to solve real world problems. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
 
+//event listener for running shoes picture
+const shoeSelector = document.getElementById('runningshoes');
+shoeSelector.addEventListener('mouseover', function() {
+    shoeSelector.style.opacity = "1";
+
+const shoeCaption = document.createElement("div");
+document.h2.append(shoeCaption);
+shoeCaption.textContent = "Running Shoe Selector"
+});
+
+shoeSelector.addEventListener('mouseout', function() {
+    shoeSelector.style.opacity = "0.25";
+})
+
+//event listener for chant book picture
+const playlistGenerator = document.getElementById("chantbook");
+playlistGenerator.addEventListener('mouseover', function() {
+    playlistGenerator.style.opacity = "1";
+});
+
+playlistGenerator.addEventListener('mouseout', function() {
+    playlistGenerator.style.opacity = "0.25";
+});
+
+
+
+const shoeCaption = document.createElement("div");
+h2.append(shoeCaption)
+shoeCaption.textContent = "Running Shoe Selector"
+
+const chantbookCaption = document.createElement("div");
+chantbookCaption.textContent = "Playlist Generator";
+
 
