92399ec5e7205424917d95865b4f12aee4a7c16b
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-22 12:10:03 -0600

    finalize left side alignment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const bio = document.createElement("div");
 document.querySelector("div").appendChild(bio)
 bio.id = "bio"
 
-bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to find a position using my new skills to solve real world problems. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
+bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to use my new skills to solve real world problems. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
 
 //event listener for running shoes picture
 const shoeSelector = document.getElementById('runningshoes');
@@ -18,6 +18,9 @@ shoeSelector.addEventListener('mouseout', function() {
 const playlistGenerator = document.getElementById("chantbook");
 playlistGenerator.addEventListener('mouseover', function() {
     playlistGenerator.style.opacity = "1";
+
+
+
 });
 
 playlistGenerator.addEventListener('mouseout', function() {
