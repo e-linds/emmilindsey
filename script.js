89a81a8f0ed0510017c5ff6cafd8c9d5ecddfbc9
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-26 20:15:19 -0600

    actual positioning of bio and topnav

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const bio = document.createElement("div");
-document.querySelector("div").appendChild(bio)
+document.querySelector("body").appendChild(bio)
 bio.id = "bio"
 
 bio.textContent = "DRAFT Hey! My name is Emmi, and I'm learning to code at Flatiron School. When I graduate, I'm hoping to use my new skills to solve real world problems. I think I'd like to be a back-end engineer but I guess we'll see, won't we! Outside of work I'm your typical Coloradan. I like to run to burn off brain fuel and try new restaurants and foods. If anyone has found some amazing Greek food in Denver, please hit me up. "
@@ -18,8 +18,6 @@ shoeSelector.addEventListener('mouseout', function() {
 const playlistGenerator = document.getElementById("chantbook");
 playlistGenerator.addEventListener('mouseover', function() {
     playlistGenerator.style.opacity = "1";
-
-
 
 });
 
